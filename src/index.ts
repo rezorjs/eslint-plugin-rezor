2cc1d2d79b3a8713a5e6c71a756260b535d672f8
Yang Mingshan <y.mingshan3@gmail.com>
2026-08-24 22:56:13 +0800

    release: v0.0.1

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ type ReactHooksFlatConfig = {
 const configs = { recommended: {} as ReactHooksFlatConfig }
 
 const plugin = {
-  meta: { name: 'eslint-plugin-rezor', version: '0.0.0' },
+  meta: { name: 'eslint-plugin-rezor', version: '0.0.1' },
   rules,
   configs,
 }
